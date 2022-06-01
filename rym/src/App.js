2c6f75171a3b505de9f85e2ea1fbb7afc98a9865
indiakka <78439130+indiakka@users.noreply.github.com>
2022-06-01 17:51:18 +0200

    event kill pasando contexto al constructor

--- rym/src/App.js
+++ rym/src/App.js
@@ -1,45 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 
-export default class CharacterCard extends Component
-{
-  constructor ( props )
-  {
-    super( props )
+export default class CharacterCard extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       state: this.props.state,
-      chapters:this.props.chapters
-    }
+      chapters: this.props.chapters,
+    };
+    this.kill = this.kill.bind( this )
+    /*  hay que pasarle a la función el concepto para
+  que el botón kill funcione*/
   }
-  componentDidMount ()
-  { /* lo utilizamos para cambiar el estado que hayamos
-    puesto en index.js*/
-    this.setState({state:"dead"})
+  kill() {
+    this.setState({ state: "Dead" });
   }
-  render ()
-  {
+  render() {
     return (
       <div className="card">
         <App titulo={this.props.titulo} />
         <p>{this.state.state}</p>
         <p>{this.props.gender}</p>
         <p>{this.state.chapters}</p>
+        <button onClick={this.kill}>KILL</button>
       </div>
     );
-  };
-}
-  
-
-export class App extends Component {
-  render ()
-  {
-    // nos devuelve algo
-    return (
-      <div className="rym">
-        <h1>{this.props.titulo}</h1> 
-      </div> /* ponemos this.props para que ponga el titulo "R y M"
-      ; pasamos de padre a hijo la props this.props.titulo */
-   )
   }
 }
 
+export class App extends Component {
+  render() {
+    // nos devuelve algo
+    return (
+      <div className="rym">
+        <h1>{this.props.titulo}</h1>
+      </div> /* ponemos this.props para que ponga el titulo "R y M"
+      ; pasamos de padre a hijo la props this.props.titulo */
+    );
+  }
+}
