33976ad42b20a75aff7d0414480baa285840794a
indiakka <78439130+indiakka@users.noreply.github.com>
2022-06-01 16:30:36 +0200

    ciclo de vida de los componentes

--- rym/src/App.js
+++ rym/src/App.js
@@ -5,11 +5,39 @@ export default class CharacterCard extends Component {
   constructor ( props )
   {
     super( props )
+    console.log( "constructor" );
+    this.state={app:true}
+  }
+  componentDidMount ()
+  {
+      this.setState({app: false})
+      console.log("did mount");
+    } 
+    componentDidUpdate( prevProps, prevState, snapshot ){
+      console.log("did update");
+}
+    componentWillUnmount() {
+console.log("will mount"); 
+    }
+    shouldComponentUpdate( nextProps, nextState ){
+      console.log( "should?" );
+      return true
+    }
+    static getDerivedStateFromProps ( props, state )
+    {
+      console.log("getDerivedStateFromProps");
+    return null
+    }
+    getSnapshotBeforeUpdate ( prevProps, prevState )
+    {
+      console.log("getSnapshotBeforeUpdate");
+    }
     render ()
     {
       console.log("render");
       return (
         <div className="card">
+        {this.state.app ? <App /> : <span></span>}  
           <h2>Nombre personaje</h2>
           <p>Estado</p>
           <p>Genero</p>
@@ -21,6 +49,14 @@ export default class CharacterCard extends Component {
   
 
 export class App extends Component() {
+  componentDidMount ()
+  {
+    this.setState( { app: false } )
+    console.log('APP: did mount')
+  }
+  componentWillUnmount() {
+  console.log("APP: will mount"); 
+      }
     render() {
       // nos devuelve algo
       return (
