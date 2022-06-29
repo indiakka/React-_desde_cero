45efa66d5526f8be35faa39b5fed3003d5ad8c72
indiakka <78439130+indiakka@users.noreply.github.com>
2022-06-29 15:47:03 +0200

    redux actions, reducers, store

--- rym/src/index.js
+++ rym/src/index.js
@@ -5,10 +5,54 @@ import { Routes, BrowserRouter, Route} from "react-router-dom";
 
 import Characters from "./componentes/Characters";
 import Char from "./componentes/Char";
+import ChForm from "./componentes/Form"
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Error404 from "./componentes/Error404";
 import {Heading, Foot} from "./componentes/Common";
+
+//Redux
+import {createStore} from 'redux'
+
+const initialState = {
+  Characters: []
+}
+
+//Acciones
+function addChar (name, state, gender, chapters){
+  return {
+    type: "ADD_CHAR",
+    payload: {
+      name: name,
+      state: state,
+      gender: gender,
+      chapters: chapters
+    }
+  }
+}
+//Reducers
+ 
+function appReducer ( state = {}, action )
+{
+  switch ( action.type )
+  {
+    case "ADD_CHAR": {
+      let newst = { ...state }
+      let ch ={ ...action.payload, id: newst.characters.length +1}
+      newst.characters.push( ch )
+      return newst
+    }
+      default: return state
+  }
+}
+
+//Store
+
+const store = createStore(appReducer, initialState)
+window.store = store
+
+//End Redux
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,6 +63,7 @@ root.render(
       <Route path="/" element={<Characters />} />
       <Route path="/personajes" element={<Characters />} />
       <Route path="/personaje/:id" element={<Char />} />
+      <Route path="/personaje/añadir" element={<ChForm />} />
       <Route element={<Error404 />} />
     </Routes>
   </BrowserRouter>
